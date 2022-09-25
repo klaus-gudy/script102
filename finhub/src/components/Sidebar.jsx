f641b6de218891c9bc5f0ef7a8826074b38fb5fd
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2022-09-25 22:05:48 +0300

    proper folder structure and icons

---
+++ finhub/src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaBeer } from 'react-icons/fa';
+
+function Sidebar() {
+  return (
+    <div className='text-white'>
+      Sidebar <FaBeer />
+    </div>
+  )
+}
+
+export default Sidebar
