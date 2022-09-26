b8faa7df6cc6ba52a80efdf0dc7d81cdb1f972bb
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2022-09-26 19:43:01 +0300

    FEAT:75% completed side bar

--- finhub/src/components/Sidebar.jsx
+++ finhub/src/components/Sidebar.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-import { FaBeer } from 'react-icons/fa';
+
+import { AiFillHome } from "react-icons/ai";
+
+const Menus = [
+  {title:'Home', src:'home'},
+  {title:'Add Customer', src:'home'},
+  {title:'View Customer', src:'home'},
+  {title:'Settings', src:'home'},
+  {title:'Logout', src:'home'},
+]
 
 function Sidebar() {
   return (
-    <div className='text-white'>
-      Sidebar <FaBeer />
+    <div className='w-72 bg-slate-400 h-screen p-5 pt-8'>
+      <h1 className='font-extrabold '>sidebar</h1>
+      <ul className='pt-6'>
+        {Menus.map(
+          (menu,index)=>(
+            <li key={index} className='text-sm cursor-pointer hover:bg-slate-300 items-center gap-x-4 p-2 flex rounded-md'>
+              <span className='float-left text-xl block'><AiFillHome/></span>
+              <span>{menu.title}</span>
+            </li>
+          )
+          )}
+      </ul>
     </div>
   )
 }
