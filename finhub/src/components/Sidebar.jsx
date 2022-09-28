64ea506970cf6b1ddd59cbbc6e8b2c83e53793b1
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2022-09-28 21:47:30 +0300

    adding react dom router for side bar

--- finhub/src/components/Sidebar.jsx
+++ finhub/src/components/Sidebar.jsx
@@ -1,30 +1,31 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 import { AiFillSetting ,AiFillHome} from "react-icons/ai";
 import { BsFillPersonPlusFill , BsFillPersonLinesFill} from "react-icons/bs";
 import { BiLogIn } from "react-icons/bi";
 
 const Menus = [
-  {title:'Home', icon:<AiFillHome/>},
-  {title:'Add Customer', icon: <BsFillPersonPlusFill/> },
-  {title:'View Customer', icon:<BsFillPersonLinesFill/>},
-  {title:'Settings', icon:<AiFillSetting/>},
-  {title:'Logout', icon:<BiLogIn/>},
+  {title:'Home', icon:<AiFillHome/>, link:'/'},
+  {title:'Add Customer', icon: <BsFillPersonPlusFill/> , link:'/add'},
+  {title:'View Customer', icon:<BsFillPersonLinesFill/>, link:'/customers'},
+  {title:'Settings', icon:<AiFillSetting/>, link:'/settings'},
+  {title:'Logout', icon:<BiLogIn/>, link:'/logout'},
 ]
 
 function Sidebar() {
   return (
-    <div className='w-72 bg-slate-400 h-screen p-5 pt-8'>
+    <div className='w-72 bg-slate-400 p-5 min-h-screen pt-8'>
       <h1 className='font-extrabold '>sidebar</h1>
       <ul className='pt-6 mt-10 space-y-4'>
-        {Menus.map(
-          (menu,index)=>(
-            <li key={index} className='text-sm cursor-pointer hover:bg-slate-300 items-center gap-x-4 p-2 flex rounded-md'>
-              <span className='float-left text-xl'>{menu.icon}</span>
-              <span>{menu.title}</span>
-            </li>
-          )
-          )}
+      {Menus.map(
+        (menu,index)=>(
+          <Link key={index} to={menu.link}  className='text-sm cursor-pointer hover:bg-slate-300 items-center gap-x-4 p-2 flex rounded-md'>
+            <span className='float-left text-xl'>{menu.icon}</span>
+            <span>{menu.title}</span>
+          </Link>
+        )
+      )}
       </ul>
     </div>
   )
