717699836bbbf2ec1e1009cb6dfddf2eae046f22
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2022-09-30 23:44:34 +0300

    completed customer add UI

--- finhub/src/pages/Add.js
+++ finhub/src/pages/Add.js
@@ -2,38 +2,29 @@ import React from 'react'
 
 function Add() {
   return (
-    <div className='p-7 flex-1 h-screen '>
-      <h1 className="text-lg font-extrabold mb-7">CREATE A NEW CUSTOMER</h1>
-      <form className="mt-4 bg-stone-600 flex flex-row  justify-around p-16 h-[80%] ">
-        <div className="flex flex-col bg-stone-400 justify-around p-7">
-          <div className="flex flex-row">
-            <label>password</label>
-            <input type="text"></input>
+    <div className="p-7 flex-1 h-screen ">
+      <h1 className="text-lg text-center font-bold mb-7">ADD CUSTOMER</h1>
+      <div className='flex  items-center justify-center  px-4 sm:px-6 lg:px-8'>
+        <div className='w-full max-w-md space-y-8  bg-white rounded-lg px-12 block'>
+        <form className='mt-8 space-y-6'>
+          <div>
+          <input type="text" placeholder="First name" className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'></input>
           </div>
-          <div className="flex flex-row">
-            <label>password</label>
-            <input type="text"></input>
+          <div>
+          <input type="text" placeholder="last name" className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'></input>
           </div>
-          <div className="flex flex-row">
-            <label>password</label>
-            <input type="text"></input>
+          <div>
+          <input type="email" placeholder="email" className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'></input>
           </div>
+          <div>
+          <input type="password" placeholder="password" className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'></input>
+          </div>
+          <div class=" py-3 ">
+              <button type="submit" class="px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Add Customer</button>
+          </div>
+        </form>
         </div>
-        <div className="flex flex-col bg-stone-400 justify-around p-7">
-          <div className="flex flex-row">
-            <label>password</label>
-            <input type="text"></input>
-          </div>
-          <div className="flex flex-row">
-            <label>password</label>
-            <input type="text"></input>
-          </div>
-          <div className="flex flex-row">
-            <label>password</label>
-            <input type="text"></input>
-          </div>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
